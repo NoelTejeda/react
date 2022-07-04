57622c4f1efb2a8a642f91a0985e7a424de2b842
noel <noelati@gmail.com>
2022-07-03 23:59:22 -0400

    proyecto-testimonio

---
+++ testimonios-freecodecamp/src/componentes/Testimonio.jsx
@@ -0,0 +1,16 @@
+// importamos react y su libreria
+import React from "react";
+
+//componente funcional
+function Testimonio() {
+  return (
+    <div className="contenedor-testimonio">
+      <img
+        className="imagen-testimonio"
+        src="../imagenes/Emma Bostian.png"
+        alt="Emma Bostian" />
+    </div>
+  );
+}
+
+//minuto 1:39:57
